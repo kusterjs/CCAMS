bb2e74ff0ebe4ae0af73603d96cc73c63fc532f1
kusterjs <jonas@kusternet.ch>
2025-10-26 23:00:24 +0100

    colour for duplicate squawk changed to redundant

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #define VER_MAJOR 2
 #define VER_MINOR 5
-#define VER_PATCH 2
-#define VER_BUILD 15
-#define VER_FILEVERSION 2,5,2,15
-#define VER_FILEVERSION_STR "2.5.2\0"
+#define VER_PATCH 3
+#define VER_BUILD 0
+#define VER_FILEVERSION 2,5,3,0
+#define VER_FILEVERSION_STR "2.5.3\0"
